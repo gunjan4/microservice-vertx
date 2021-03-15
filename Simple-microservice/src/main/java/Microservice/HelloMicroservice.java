7990eab58988e65f54f70a4ba173e2c06805ebf0
Gunjan <gunjan \.kothari4@gmail.com>
2021-03-15 12:40:18 +0000

    Separated the simple microservice

---
+++ Simple-microservice/src/main/java/Microservice/HelloMicroservice.java
@@ -0,0 +1,36 @@
+package Microservice;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+
+public class HelloMicroservice extends AbstractVerticle {
+
+    @Override
+    public void start() {
+     Router router = Router.router(vertx);
+         router.get("/").handler(this::hello);
+         router.get("/:name").handler(this::hello);
+
+         vertx.createHttpServer()
+             .requestHandler(router::accept)
+             .listen(8080);
+    }
+
+
+
+    private void hello(RoutingContext rc){
+         String message = "hello";
+
+         if(rc.normalisedPath() != null ){
+             message += rc.normalisedPath();
+         }
+
+         JsonObject json = new JsonObject().put("message", message);
+         rc.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+         .end(json.encode());
+        }
+}
+
